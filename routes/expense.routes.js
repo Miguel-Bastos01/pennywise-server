0015631b3347eafb83a26857d3ea3f3acd97dba4
Miguel Bastos <miguel.alexandre.bastos01@gmail.com>
2025-07-05 09:35:59 +0100

    Adapted the post expense to better mirror code used across the rest of the backend

--- routes/expense.routes.js
+++ routes/expense.routes.js
@@ -5,24 +5,23 @@ const Expense = require("../models/Expense.model.js")
 
 
 router.get("/expenses", isAuthenticated, (req, res, next )=> {
-   
- Expense.find({user: req.payload._id})
- 
+   Expense.find({user: req.payload._id})
     .sort({createdAt: -1})
     .then ((allUserExpenses) => res.json(allUserExpenses))
     .catch ((err) => {next (err)})
 });
+ 
+   
 
 
-router.post("/expenses", isAuthenticated, async (req, res, next) =>{
-  const {user, category, amount, title, notes, mood} = req.body
+router.post("/expenses", isAuthenticated, (req, res, next) =>{
+  const {category, amount, title, notes, mood} = req.body
   const userId = req.payload._id
 
   if (!category ||!amount  ||!title ){
    return res.status(400).json({ message: "Missing required field"})
       }
-      try {
-        const expense = await Expense.create({
+      Expense.create({
             user: userId,
             category,
             amount,
@@ -30,13 +29,12 @@ router.post("/expenses", isAuthenticated, async (req, res, next) =>{
             notes,
             mood,
         })
-        res.status(201).json(expense)
-        console.log("Expense created successfully")
-      }
-      catch(err){
-        next(err)
-      }
-
-  
-  })
+          .then((expense) => {
+            res.status(201).json(expense)
+            console.log("Expense created successfully")
+          })
+          .catch((err) => {
+            next (err)
+          })
+})
   module.exports = router
