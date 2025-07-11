b4058d896e08a4645461be2da7ab52827c8f0d86
Miguel Bastos <miguel.alexandre.bastos01@gmail.com>
2025-07-11 22:15:04 +0100

    Deployment step 1 Mongo

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const app = require("./app");
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+  });
